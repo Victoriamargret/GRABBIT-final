6eb58113e6501906505317c7f27ffe71fefcf0c3
Victoria <victoriamargaret22@gmail.com>
2017-06-24 12:29:28 +0530

    Update MainActivity.java

--- GRabbit2/app/src/main/java/com/example/vik/grabbit/MainActivity.java
+++ GRabbit2/app/src/main/java/com/example/vik/grabbit/MainActivity.java
@@ -299,6 +299,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onPause(){
         super.onPause();
         th.cancel();
+            Start.mp1.stop();
         finish();
 
     }
